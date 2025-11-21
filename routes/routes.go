440c7f24d979aec0ed3b872ae9b600ce95fce35b
d.alves <d.alves@unlimint.com>
2025-11-20 21:32:33 -0300

    Add middleware

--- routes/routes.go
+++ routes/routes.go
@@ -4,11 +4,13 @@ import (
 	"net/http"
 
 	"github.com/DenilsonNil/api-go-rest/controller"
+	"github.com/DenilsonNil/api-go-rest/controller/middleware"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequests() {
 	r := mux.NewRouter()
+	r.Use(middleware.SetContentTypeJSON)
 	r.HandleFunc("/", controller.Home)
 	r.HandleFunc("/api/personalidades", controller.AllPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades", controller.Create).Methods("Post")
